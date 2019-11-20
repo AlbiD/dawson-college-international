862e639f600102cfef42407dd7bfdbb9deba3989
Albi Delibashi <delibashialbi@gmail.com>
2019-11-20 13:53:03 -0700

    dawson community collge

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import moment from "moment";
+import Logo from "../../static/assets/logo/dcclogosmall.jpg";
 
 export default class App extends Component {
   render() {
     return (
-      <div className='app'>
+      <div className='home-page-container'>
+        <img src={Logo} alt="website-logo"/>
         <h1>DevCamp React Starter</h1>
         <h2>React Redux Router</h2>
+        <div>{moment().format('MMMM Do YYYY, h:mm:ss a')}</div> 
       </div>
     );
   }
---
+++ src/components/portfolio/portfolio-container.js
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+import axios from 'axios';
+
+import PortfolioItem from "./portfolio-item"
+
+export default class PortfolioContainer extends Component {
+    constructor() {
+        super();
+        
+
+        };
+    }
+
